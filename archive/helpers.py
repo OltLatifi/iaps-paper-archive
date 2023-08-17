4c27366b48c3aac666c30d4b301a9a3af45e0aac
OltLatifi <44296923+OltLatifi@users.noreply.github.com>
2023-08-17 13:17:09 +0200

    search (#4)
    * pagination & parsed date
    
    * search implemented

--- archive/helpers.py
+++ archive/helpers.py
@@ -1,4 +1,7 @@
-def pagination_metadata(paginator, results):
+from .models import Paper, Author, Category
+from django.db.models import Q
+
+def pagination_metadata(paginator, results: dict):
     """
     Adds pagination metadata to the result
     """
@@ -10,4 +13,45 @@ def pagination_metadata(paginator, results):
             'current_page': paginator.page.number,
             'last_page': paginator.page.paginator.num_pages,
             'results': results
-        }   
+        }
+
+def search_paper(search: str):
+    """
+    Filters papers based on title, abstract, author's name and categorie's name
+    """
+    papers = Paper.objects.filter(
+        Q(title__contains=search) |
+        Q(abstract__contains=search) |
+        Q(categories__name__contains=search) |
+        Q(authors__name__contains=search)
+    )
+
+    return papers
+
+def search_author(search: str):
+    """
+    Filters authors based on name, their paper's abstract, thier paper's abstract
+    and their paper's categorie's name
+    """
+    authors = Author.objects.filter(
+        Q(name__contains=search) |
+        Q(paper__title__contains=search) |
+        Q(paper__abstract__contains=search) |
+        Q(paper__categories__name__contains=search)
+    )
+
+    return authors
+
+def search_category(search: str):
+    """
+    Filters categories based on name, their paper's abstract, thier paper's abstract
+    and their paper's categorie's name
+    """
+    categories = Category.objects.filter(
+        Q(name__contains=search) |
+        Q(paper__title__contains=search) |
+        Q(paper__abstract__contains=search) |
+        Q(paper__categories__name__contains=search)
+    )
+
+    return categories
