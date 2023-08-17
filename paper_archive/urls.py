8f27fbb3bc4713c854c843c3eebf0be4986eac8a
OltLatifi <oltlatifi2003@gmail.com>
2023-08-17 20:29:37 +0200

    sort, refactor and documentation

--- paper_archive/urls.py
+++ paper_archive/urls.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+        title="Archive API",
+        default_version='v1',
+        description="The paper archive requeste by the i-aps team",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("", include("archive.urls")),
     path("admin/", admin.site.urls),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
